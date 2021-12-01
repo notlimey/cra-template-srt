f99d147469ff789af8d7e6547b32c6ef647adc95
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2021-12-01 19:20:12 +0100

    Made welcome page

--- template/src/Pages/Home.tsx
+++ template/src/Pages/Home.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Logo from '@Images/LimeyfyLogo.png'
 
 const Home = () => {
     return (
         <React.Fragment>
-            <h1 className="text-center">Welcome to React!</h1>
-            <p className="text-center">Thanks for using limeyfy react template :)</p>
+            <div className="text-center welcome-app">
+                <h1>Welcome to React!</h1>
+                <p>Thanks for using <code>Limeyfy</code> react template :)</p>
+                <div className="spinning-logo">
+                    <img src={Logo} alt="Limeyfy logo" />
+                </div>
+            </div>
         </React.Fragment>
     );
 }
