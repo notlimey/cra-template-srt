ecc639b49b644e80620177316c7ca12d8f9deb14
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2021-12-04 12:58:02 +0100

    Default helmet, Made by

--- template/src/Components/DefaultHelmet.tsx
+++ template/src/Components/DefaultHelmet.tsx
@@ -1,9 +1,54 @@
+import React from "react";
 import { Helmet } from "react-helmet";
+import FavIcon from '@Assets/favicon.ico';
 
-const DefaultHelmet = () => {
+// Tilte = Websites Title
+// SubTitle = is the subtitle E.g Title | Subtitle
+// Description = A simple description of the website
+
+const WebsiteUrl = window.location.href; // Full url of the website E.g https://limeyfy.no
+const DefaultTitle = "Test app"; // Default title incase you dont want to set the title every time you use the DefaultHelmet
+const ThemeColor = "#000000" // The theme color of the website
+const DefaultDescription = 
+    `This is a react application made with React-Typescript structured template (https://github.com/NotLimey/React-Typescript-Structured-Template)`
+
+const DefaultHelmet = ({Title, Description, SubTitle, MetaImage} : any) => {
+
     return (
         <Helmet>
-            <title>Welcome to react!</title>
+            {/* Primary Meta Tags */}
+            <title>{SubTitle ? `${Title ? Title : DefaultTitle} | ${SubTitle}` : `${Title}`}</title>
+            <meta name="title" content={SubTitle ? `${Title ? Title : DefaultTitle} | ${SubTitle}` : `${Title}`} />
+            <meta name="application-name" content={SubTitle ? `${Title ? Title : DefaultTitle} | ${SubTitle}` : `${Title}`} />
+
+            <meta name="description" content={Description ? Description : DefaultDescription} />
+
+            {/* Open Graph / Facebook */}
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content={WebsiteUrl}  />
+            <meta property="og:title" content={SubTitle ? `${Title ? Title : DefaultTitle} | ${SubTitle}` : `${Title}`} />
+            <meta property="og:description" content={Description ? Description : DefaultDescription} />
+            <meta property="og:image" content={MetaImage} />
+
+            {/* Twitter */}
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:url" content={WebsiteUrl} />
+            <meta property="twitter:title" content={SubTitle ? `${Title ? Title : DefaultTitle} | ${SubTitle}` : `${Title}`} />
+            <meta property="twitter:description" content={Description ? Description : DefaultDescription} />
+            <meta property="twitter:image" content={MetaImage}></meta>
+
+            {/* Apple */}
+            <meta name="apple-mobile-web-app-title" content={SubTitle ? `${Title ? Title : DefaultTitle} | ${SubTitle}` : `${Title}`} />
+	
+            <link rel="apple-touch-icon" sizes="180x180" href={FavIcon} ></link>
+
+            {/* FavIcons */}
+            <link rel="shortcut icon" href={FavIcon} />
+            <link rel="icon" type="image/png" sizes="32x32" href={FavIcon} />
+            <link rel="icon" type="image/png" sizes="16x16" href={FavIcon}></link>
+
+            {/* Theme Color */}
+	        <meta name="theme-color" content={ThemeColor}></meta>
         </Helmet>
     );
 }
--- template/src/Pages/Home.tsx
+++ template/src/Pages/Home.tsx
@@ -5,11 +5,14 @@ const Home = () => {
     return (
         <React.Fragment>
             <div className="text-center welcome-app">
-                <h1>Welcome to React!</h1>
-                <p>Thanks for using <code>Limeyfy</code> react template :)</p>
+                <h1>Welcome to React-Typescript</h1>
+                <p>Thanks for using <code>React-Typescript Structured Template!</code></p>
                 <div className="spinning-logo">
                     <img src={Logo} alt="Limeyfy logo" />
                 </div>
+                <p style={{marginTop: '200px'}}>Made with 
+                    <svg style={{margin: '0px 5px'}} role="img" xmlns="http://www.w3.org/2000/svg" width="18px" height="18px"   viewBox="0 0 24 24" aria-labelledby="favouriteIconTitle" stroke="#e31b23" stroke-width="1" strokeLinecap="square" strokeLinejoin="miter" fill="#e31b23" color="#e31b23"> <title id="favouriteIconTitle">Favourite</title> <path d="M12,21 L10.55,19.7051771 C5.4,15.1242507 2,12.1029973 2,8.39509537 C2,5.37384196 4.42,3 7.5,3 C9.24,3 10.91,3.79455041 12,5.05013624 C13.09,3.79455041 14.76,3 16.5,3 C19.58,3 22,5.37384196 22,8.39509537 C22,12.1029973 18.6,15.1242507 13.45,19.7149864 L12,21 Z"/> </svg>
+                By <a style={{color: '#f1f1f1'}} href="https://github.com/Notlimey" target="_blank" rel="noreferrer">Limey</a></p>
             </div>
         </React.Fragment>
     );
