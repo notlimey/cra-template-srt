0b35afe23911856bc19104aeaae6bdd8e1dc840c
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2021-12-04 18:43:03 +0100

    Update CombineClasses.ts
    Renamed link in file

--- template/src/Helpers/CombineClasses.ts
+++ template/src/Helpers/CombineClasses.ts
@@ -1,4 +1,4 @@
-// Documentation on component is here: https://github.com/Notlimey/SRT-template#combindclasses
+// Documentation on component is here: https://github.com/Notlimey/SRT-template#combineClasses
 
 export default function CombineClasses (a : string, b : string | undefined) {
     return `${a}${b ? ` ${b}` : ''}`;
