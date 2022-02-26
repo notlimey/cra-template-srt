fac814a4c0d193415b769897fdd4e6a72b5f4417
Martin Myhre <70258183+martinlime@users.noreply.github.com>
2022-02-26 14:33:45 +0100

    Removed nl-ui

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,14 +1,12 @@
 import '@scss/main.scss'
 import React from 'react';
 import { Route, Routes } from 'react-router'
-import { DefaultHelmet } from 'nl-ui';
 import Home from '@pages/Home';
 import NotFound from '@pages/NotFound';
 
 function App() {
   return (
     <React.Fragment>
-      <DefaultHelmet title="SRT" />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
--- template/src/Pages/NotFound.tsx
+++ template/src/Pages/NotFound.tsx
@@ -1,13 +1,9 @@
-import { DefaultHelmet } from "nl-ui";
-import NotFoundComponent from "@components/NotFoundComponent";
+import NotFoundComponent from "@/components/NotFoundComponent";
 import React from "react";
 
 const NotFound = () => {
     return (
         <React.Fragment>
-            <DefaultHelmet 
-                title="404 Not found"
-            />
             <NotFoundComponent />
         </React.Fragment>
     );
